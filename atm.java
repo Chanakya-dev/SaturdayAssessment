8b2e80ff0b1cf7207d2bbb67fe97f8f7af071f10
nirajpavankumar <nirajpavankumar1282@gmail.com>
2024-06-29 10:36:49 +0530

    hii

---
+++ atm.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class atm{
+    public double  m1(double balance,double withdraw) {
+        if (balance > withdraw) {
+            System.out.println("transaction sucessful");
+        }
+   
+    
+    return balance ;
+    }
+public static void main(String[] args) {
+    Scanner in  = new Scanner(System.in);
+    System.out.println("Enter your balance");
+    double  balance = in .nextDouble();
+    Scanner hi = new Scanner(System.in);
+    System.out .println("enter the balamce ");
+    double  withdraw = hi .nextDouble();
+    atm obj = new atm();
+    obj.m1(balance,withdraw);
+
+}
